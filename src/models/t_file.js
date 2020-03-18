c9bd39620d43c740b87b6101cbc9db6b88a61de4
bin.gong <bin.gong@eazytec.com>
2020-03-18 10:53:52 +0800

    orm

---
+++ src/models/t_file.js
@@ -0,0 +1,51 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('t_file', {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    c_type: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false
+    },
+    c_path: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    c_name: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    c_link: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true
+    },
+    c_link_type: {
+      type: DataTypes.STRING(50),
+      allowNull: true
+    },
+    c_status: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true
+    },
+    c_user: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true
+    },
+    c_time: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    c_memo: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    }
+  }, {
+    tableName: 't_file'
+  });
+};
